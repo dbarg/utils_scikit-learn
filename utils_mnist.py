9210cf20459abc9a498cabf2d0a46bec49912011
dbarg <derek.barge@fysik.su.su>
2019-12-01 11:46:45 -0600

    work in progress

---
+++ utils_mnist.py
@@ -0,0 +1,37 @@
+
+#------------------------------------------------------------------------------
+# Scikit 0.18.1 OpenML - Broken
+#------------------------------------------------------------------------------
+
+#from sklearn.datasets import fetch_openml
+#mnist = fetch_openml('mnist_784', data_home='./')
+#ImportError: cannot import name 'fetch_openml'
+     
+    
+#------------------------------------------------------------------------------
+# Scikit 0.18.1 Fetch - Broken
+#------------------------------------------------------------------------------
+
+#from sklearn.datasets import fetch_mldata
+#mnist = fetch_mldata('MNIST original', data_home='./')
+
+
+#------------------------------------------------------------------------------
+# Tensorflow - Broken
+#------------------------------------------------------------------------------
+
+#from tensorflow.examples.tutorials.mnist import input_data
+#mnist = input_data.read_data_sets('~./mnist', one_hot=True)
+
+    
+#------------------------------------------------------------------------------
+# Tensorflow - Broken
+#------------------------------------------------------------------------------
+
+#from keras.datasets import mnist
+#(X_train, y_train), (X_test, y_test) = mnist.load_data()
+#Exception: URL fetch failure on https://s3.amazonaws.com/img-datasets/mnist.npz: None -- [Errno 110] Connection timed out
+
+
+
+
